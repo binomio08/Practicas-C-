25885e5ec8800307cf4d9a6fb8a8d1bda14d0079
Leandro Troncoso <binomio08@hotmail.com>
2023-03-21 00:53:01 -0300

    unidad5

---
+++ unidad5/ejemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿// Hacemos un  ciclo for. 
+// Diseñamos un programa que pregunte 10 nros y te de un promedio
+
+
+
+// Usamos ciclo for
+
+// for(int i = 0; i < 5; i++){
+//     Console.WriteLine("Hola Mundo " + (i + 1));
+// }
+
+
+//Otro ejemplo con for
+
+int n = 0, promedio, suma = 0;
+
+
+for (int i = 0; i < 5; i++)
+{    
+Console.WriteLine("Ingrese un nro:" );
+n = int.Parse(Console.ReadLine()??"0");
+suma =+ n;
+}
+promedio = suma / 5;
+Console.WriteLine("EL promedio es: " + n);
