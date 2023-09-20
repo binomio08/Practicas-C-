3014e8b3a4ab9d8107c5f74675acd3f81586d1cd
Leandro Troncoso <binomio08@hotmail.com>
2023-09-20 16:40:08 -0300

    Arreglo2

--- TPFinal_Troncoso/Program.cs
+++ TPFinal_Troncoso/Program.cs
@@ -4,28 +4,38 @@
 //c. El menor de los números primos.
 //Nota: evaluar el uso de una función que analice si un número dado es primo o no y que devuelva true o false según corresponda.
 
-//Declaro variables globales 
-int nro;
-
-//Solicito datos
-Console.WriteLine("Ingrese un nro...");
-nro = int.Parse(Console.ReadLine() ?? "0");
-
-// Evaluo el dato ingrasado a travez de while, mientras el dato no sea igual a 0 el ciclo continua pidiendo datos
-while (nro != 0)
+internal class Program
 {
-    // vuelvo a solicitar datos dentro del bucle while
-    Console.WriteLine("Ingrese otro nro...");
-    nro = int.Parse(Console.ReadLine() ?? "0");
-}
-
-// Declaro una función llamada primos que evalua si el número ingrasado es primo(devuelve un true) o no(devuelve un false) 
-static bool primos(int a){
-    int cont = 0;
-    for (int i = 1; i <= a; i++)
+    private static void Main(string[] args)
     {
-        if(a % i == 0) cont++;
+        //Declaro variables globales 
+        int nro;
+
+        //Solicito datos
+        Console.WriteLine("Ingrese un nro...");
+        nro = int.Parse(Console.ReadLine() ?? "0");
+
+        // Evaluo el dato ingrasado a travez de while, mientras el dato no sea igual a 0 el ciclo continua pidiendo datos
+        while (nro != 0)
+        {
+            // vuelvo a solicitar datos dentro del bucle while
+            Console.WriteLine("Ingrese otro nro...");
+            nro = int.Parse(Console.ReadLine() ?? "0");
+        }
+
+        Console.WriteLine();
+
     }
-    if(cont == 2) return true;
-    else return false;
+
+    // Declaro una función llamada primos que evalua si el número ingrasado es primo(devuelve un true) o no(devuelve un false) 
+    static bool primos(int a)
+    {
+        int cont = 0;
+        for (int i = 1; i <= a; i++)
+        {
+        if (a % i == 0) cont++;
+        }
+        if (cont == 2) return true;
+        else return false;
+    }
 }
